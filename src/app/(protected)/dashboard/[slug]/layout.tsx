ea949ba4d9198032ae823a62ed219c9d26c69768
Kaarti22 <kaartimondi@gmail.com>
2024-12-20 17:51:12 +0530

    infobar updated

--- src/app/(protected)/dashboard/[slug]/layout.tsx
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -17,6 +17,7 @@ const layout: React.FC<LayoutProps> = ({ children, params }) => {
       {/* Infobar */}
       <div className="lg:ml-[250px] lg:pl-10 lg:py-5 flex flex-col overflow-auto">
         <Infobar slug={params.slug} />
+        {children}
       </div>
     </div>
   );
