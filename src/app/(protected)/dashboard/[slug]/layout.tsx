7c12ac6af86d2886f3f8968f80df6ff2d661d24a
Kaarti22 <kaartimondi@gmail.com>
2024-12-31 01:25:41 +0530

    prefetch integrations and automations

--- src/app/(protected)/dashboard/[slug]/layout.tsx
+++ src/app/(protected)/dashboard/[slug]/layout.tsx
@@ -1,25 +1,40 @@
-import Infobar from "@/components/global/Infobar";
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from "@tanstack/react-query";
+import Infobar from "@/components/global/infobar";
 import Sidebar from "@/components/global/sidebar";
 import React from "react";
+import {
+  prefetchUserAutomations,
+  PrefetchUserProfile,
+} from "@/react-query/prefetch";
 
 interface LayoutProps {
   children: React.ReactNode;
   params: { slug: string };
 }
 
-const layout: React.FC<LayoutProps> = ({ children, params }) => {
-  // QueryClient fetch data
+const layout: React.FC<LayoutProps> = async ({ children, params }) => {
+  const query = new QueryClient();
+
+  await PrefetchUserProfile(query);
+
+  await prefetchUserAutomations(query);
 
   return (
-    <div className="p-3">
-      {/* Sidebar */}
-      <Sidebar slug={params.slug} />
-      {/* Infobar */}
-      <div className="lg:ml-[250px] lg:pl-10 lg:py-5 flex flex-col overflow-auto">
-        <Infobar slug={params.slug} />
-        {children}
+    <HydrationBoundary state={dehydrate(query)}>
+      <div className="p-3">
+        {/* Sidebar */}
+        <Sidebar slug={params.slug} />
+        {/* Infobar */}
+        <div className="lg:ml-[250px] lg:pl-10 lg:py-5 flex flex-col overflow-auto">
+          <Infobar slug={params.slug} />
+          {children}
+        </div>
       </div>
-    </div>
+    </HydrationBoundary>
   );
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import { Toaster } from "sonner";
+import ReactQueryProvider from "@/providers/react-query-provider";
 
 const jakarta = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
@@ -26,8 +27,8 @@ export default function RootLayout({
             defaultTheme="system"
             disableTransitionOnChange
           >
-            {children}
-            <Toaster/>
+            <ReactQueryProvider>{children}</ReactQueryProvider>
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
