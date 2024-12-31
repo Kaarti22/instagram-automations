f207f7551b05c4c68c0886f9fa3832d5449bc4b9
Kaarti22 <kaartimondi@gmail.com>
2024-12-31 16:03:11 +0530

    fetching all the automations

--- src/actions/automations/index.ts
+++ src/actions/automations/index.ts
@@ -1,9 +1,9 @@
 "use server";
 
 import { onCurrentUser } from "../user";
-import { createAutomation } from "./queries";
+import { createAutomation, getAutomations } from "./queries";
 
-export const getAllAutomations = async () => {
+export const createAutomations = async () => {
   const user = await onCurrentUser();
 
   try {
@@ -12,5 +12,17 @@ export const getAllAutomations = async () => {
     return { status: 404, data: "Oops! Something went wrong" };
   } catch (error) {
     return { status: 500, data: "Internal server error" };
+  }
+};
+
+export const getAllAutomations = async () => {
+  const user = await onCurrentUser();
+
+  try {
+    const automations = await getAutomations(user.id);
+    if (automations) return { status: 200, data: automations.automations };
+    return { status: 404, data: [] };
+  } catch (error) {
+    return { status: 500, data: [] };
   }
 };
